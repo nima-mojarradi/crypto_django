f759cfb78de60f15549b6616549f7ef796323677
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-10-20 07:46:46 +0330

    feat:settings for cache added

--- bitcoin/settings.py
+++ bitcoin/settings.py
@@ -123,16 +123,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CACHES = {
-#  "default": {
-#  "BACKEND": "django_redis.cache.RedisCache",
-#  "LOCATION": "redis://127.0.0.1:6379/1",
-#  "OPTIONS": {
-#  "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#  }
-#  },
-#  "auth": {
-#  "BACKEND": "django_redis.cache.RedisCache",
-#  "LOCATION": "redis://127.0.0.1:6379/1",
-# }
-# }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/3",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
